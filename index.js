1c288288a5477fda33b9fd8278f48838689aea65
William MacDonald <wmsmacdonald@gmail.com>
2016-03-20 02:24:47 -0500

    client

---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+var net = require('net');
+
+//var HOST = '127.0.0.1';
+var HOST = 'fgen.quotacle.com';
+var PORT = 80;
+
+var client = new net.Socket();
+
+var startTime;
+client.connect(PORT, HOST, function() {
+  startTime = new Date().getTime();
+
+  console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+  // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
+  client.write('I am Chuck Norris!');
+
+});
+
+// Add a 'data' event handler for the client socket
+// data is what the server sent to this socket
+client.on('data', function(data) {
+
+  console.log('DATA: ' + data);
+  console.log('duration: %d', (new Date().getTime() - startTime) / 1000);
+  // Close the client socket completely
+  client.destroy();
+
+});
+
+// Add a 'close' event handler for the client socket
+client.on('close', function() {
+  console.log('Connection closed');
+});
